8d45e0531f84f11edd780c67cf408a2c49851932
Steve McCarthy <steve@redlua.com>
2018-06-30 15:31:33 -0400

    Add Index.Add test, fix improper test capitalization

--- index_test.go
+++ index_test.go
@@ -1,23 +1,59 @@
 package index
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func testIndexAdd(t *testing.T) {
+func TestIndexAdd(t *testing.T) {
+	type Case struct {
+		Index     Index
+		Documents []string
+		Terms     map[string]map[int]struct{}
+	}
+
+	cases := []Case{
+		Case{
+			Index:     Index{Terms: map[string]map[int]struct{}{}},
+			Documents: []string{"This is the beginning of the test."},
+			Terms: map[string]map[int]struct{}{
+				"this":      map[int]struct{}{0: struct{}{}},
+				"is":        map[int]struct{}{0: struct{}{}},
+				"the":       map[int]struct{}{0: struct{}{}},
+				"beginning": map[int]struct{}{0: struct{}{}},
+				"of":        map[int]struct{}{0: struct{}{}},
+				"test":      map[int]struct{}{0: struct{}{}},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		i := c.Index
+		for _, doc := range c.Documents {
+			i.Add(doc)
+		}
+
+		if !reflect.DeepEqual(i.Documents, c.Documents) {
+			t.Errorf("Got: %+v, Expected: %q", i.Documents, c.Documents)
+		}
+		if !reflect.DeepEqual(i.Terms, c.Terms) {
+			t.Errorf("Got: %+v, Expected: %q", i.Terms, c.Terms)
+		}
+	}
+}
+
+func TestIndexFindOne(t *testing.T) {
 	t.Skip()
 }
 
-func testIndexFindOne(t *testing.T) {
+func TestIndexFindAll(t *testing.T) {
 	t.Skip()
 }
 
-func testIndexFindAll(t *testing.T) {
+func TestIndexFindAny(t *testing.T) {
 	t.Skip()
 }
 
-func testIndexFindAny(t *testing.T) {
-	t.Skip()
-}
-
-func testNormalize(t *testing.T) {
+func TestNormalize(t *testing.T) {
 	t.Skip()
 }
